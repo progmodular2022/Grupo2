8680c00d098f7745845089c510d21c21992d6170
Gabriel Madeira <gabrielmadeira2002@gmail.com>
2022-06-03 09:05:41 -0300

    nome das funcoes de teste corrigidos

--- test/test_main.py
+++ test/test_main.py
@@ -5,14 +5,14 @@ path_to_module = "./src/"
 sys.path.append(path_to_module)
 
 
-def check_letter():
+def test_check_letter():
     assert main.check_letter("a", "banana") == True
     assert main.check_letter("b", "banana") == True
     assert main.check_letter("c", "banana") == False
     assert main.check_letter("d", "banana") == False
 
 
-def add_letter():
+def test_add_letter():
     assert main.add_letter("a", "banana", ["_", "_", "_", "_", "_", "_"], 0) == [
         "_", "a", "_", "a", "_", "a"]
 
@@ -23,7 +23,7 @@ def add_letter():
         "_", "_", "_", "f", "_"]
 
 
-def check_full_word():
+def test_check_full_word():
     assert main.check_full_word("banana", "banana") == True
     assert main.check_full_word("abacaxi", "abacate") == False
     assert main.check_full_word("abacaxi", "abacaxi") == True
