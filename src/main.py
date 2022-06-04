a3c9a325d98ef586c1dd6203e37bf9dc85b69269
Gabriel Madeira <gabrielmadeira2002@gmail.com>
2022-06-03 21:32:23 -0300

    jogo funcionando e com desenhos do boneco da forca

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import random
+from winsound import SND_NODEFAULT
 import login
 
 
@@ -36,8 +37,76 @@ def file_words_to_vector(words):
     return words_vector
 
 
-def main():
+def display_hangman():
+    stages = [
+        """
+                    --------
+                    |      |
+                    |
+                    |
+                    |
+                    |
+                    -
+        """,
+        """
+                    --------
+                    |      |
+                    |      0
+                    |
+                    |
+                    |
+                    -
+        """,
+        """
+                    --------
+                    |      |
+                    |      0
+                    |      |
+                    |      |
+                    |
+                    -
+        """,
+        """
+                    --------
+                    |      |
+                    |      0
+                    |     \\|
+                    |      |
+                    |
+                    -
+        """,
+        """
+                    --------
+                    |      |
+                    |      0
+                    |     \\|/
+                    |      |
+                    |
+                    -
+        """,
+        """
+                    --------
+                    |      |
+                    |      0
+                    |     \\|/
+                    |      |
+                    |     /
+                    -
+        """,
+        """
+                    --------
+                    |      |
+                    |      0
+                    |     \\|/
+                    |      |
+                    |     / \\
+                    -
+        """
+    ]
+    return stages
 
+
+def main():
     # utilizando componente reutilizavel para login
 
     usr_inp = input("Já possui conta? (s/n)\n")
@@ -66,13 +135,16 @@ def main():
     else:
         username = input("Insira o nome de usuário:\n")
 
-        with open("users.txt", "r") as users_file:
+        with open("./src/users.txt", "r") as users_file:
             for line in users_file:
                 if username == line.strip().split()[0]:
                     user_data = line.split()
                     file_username = user_data[0]
                     file_password = user_data[1]
                     break
+            else:
+                print("Usuário não existe")
+                return
 
             if login.check_username(file_username, username):
                 password = input("Insira a senha:\n")
@@ -81,68 +153,79 @@ def main():
                 else:
                     print("Senha incorreta")
                     return
-            else:
-                print("Usuário não existe")
-                return
+
     #
 
-    words = open("words.txt", "r").read()
+    words = open("./src/words.txt", "r").read()
     words_vector = file_words_to_vector(words.split("\n"))
 
     word = select_word(words_vector)[0]
     user_word = ["_" for i in range(len(word))]
 
+    stages = display_hangman()
+
     letters_found = 0
 
-    attempts = len(word) // 2
+    attempts = len(stages)
     attempts_count = 0
 
-    print("\nTotal de erros permitidos: ", attempts)
+    used_letters = []
 
     while attempts_count <= attempts:
-        print("\nErros cometidos: ", attempts_count, "/", attempts)
+        print(stages[attempts_count])
+
+        if len(used_letters) > 0:
+            print("letras já usadas:", end="")
+            for letters in used_letters:
+                print(letters, end=" ")
 
         print("\nPalavra: ", end="")
-        print(user_word)
+        for letter in user_word:
+            print(letter, end=" ")
 
-        if len(word) - letters_found > len(word) // 2:
-            user_letter = input("\nDigite uma letra: ")
+        user_letter = input("\n>Digite uma letra: ")
+        used_letters.append(user_letter)
 
-            if check_letter(user_letter, word):
-                user_word, letters_found = add_letter(
-                    user_letter, word, user_word, letters_found)
+        if check_letter(user_letter, word):
+            user_word, letters_found = add_letter(
+                user_letter, word, user_word, letters_found)
+        else:
+            print(">Letra não encontrada")
+            attempts_count += 1
+            if attempts_count == attempts:
+                print(">Número máximo de tentativas atingido\n>Você perdeu!")
+                print("Você errou, a palavra era:", word)
+                break
+
+        if letters_found == len(word):
+            print("\nPalavra: ", end="")
+            for letter in user_word:
+                print(letter, end=" ")
+
+            if check_full_word:
+                print("\nParabéns, você ganhou!")
+                break
             else:
-                print(".Letra não encontrada")
-                attempts_count += 1
-                if attempts_count == attempts:
-                    print(".Número máximo de tentativas atingido\n.Você perdeu!")
-                    print("Você errou, a palavra era:", word)
+                print("\nVocê perdeu!")
+                break
+
+        if len(word) - letters_found <= len(word) // 2:
+            print("\nPalavra: ", end="")
+            for letter in user_word:
+                print(letter, end=" ")
+            usr_choice = input(
+                "\n>falta metade das letras, quer chutar a palavra ou tentar outra letra? (chutar/letra)\n")
+
+            if usr_choice == "chutar":
+                user_word = input("Digita a palavra: ")
+                if check_full_word(user_word, word):
+                    print("\nVocê venceu!")
                     break
 
-        if len(word) - letters_found == len(word) // 2:
-
-            print("\nPalavra: ", end="")
-            print(user_word)
-
-            print("falta menos da metade das letras, agora chuta a palavra inteira")
-            while (1):
-                user_word = input("digite a palavra: ")
-
-                if len(user_word) == 1:
-                    print("agora nao pode chutar letra, só a palavra inteira")
-
-                if len(user_word) > 1:
-                    if check_full_word(user_word, word):
-                        flag = 1
-                        print("Boa, você acertou a palavra")
-                        break
-                    else:
-                        flag = 0
-                        print("Você errou, a palavra era:", word)
-                        break
-
-            if (flag or not flag):
-                break
+                else:
+                    print("\nVocê perdeu!")
+                    print("Você errou, a palavra era:", word)
+                    break
 
 
 if __name__ == "__main__":
