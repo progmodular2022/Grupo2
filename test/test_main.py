0aed079d299299eb879dc3f1bcfd82d3574cc9a1
Gabriel Madeira <gabrielmadeira2002@gmail.com>
2022-06-03 22:09:19 -0300

    agora é pros testes darem certo

--- test/test_main.py
+++ test/test_main.py
@@ -11,15 +11,10 @@ def test_check_letter():
     assert main.check_letter("d", "banana") == False
 
 
-# def test_add_letter():
-#     assert main.add_letter("a", "banana", ["_", "_", "_", "_", "_", "_"], 3) == [
-#         "_", "a", "_", "a", "_", "a"]
-
-#     assert main.add_letter("b", "banana", ["_", "_", "_", "_", "_", "_"], 1) == [
-#         "b", "_", "_", "_", "_", "_"]
-
-#     assert main.add_letter("f", "garfo", ["_", "_", "_", "_", "_"], 1) == [
-#         "_", "_", "_", "f", "_"]
+def test_add_letter():
+    assert main.add_letter("a", "banana", ["_", "_", "_", "_", "_", "_"], 0) == (["_", "a", "_", "a", "_", "a"], 3)
+    assert main.add_letter("b", "banana", ["_", "_", "_", "_", "_", "_"], 0) == (["b", "_", "_", "_", "_", "_"], 1)
+    assert main.add_letter("c", "banana", ["_", "_", "_", "_", "_", "_"], 0) == (["_", "_", "_", "_", "_", "_"], 0)
 
 
 def test_check_full_word():
@@ -29,6 +24,6 @@ def test_check_full_word():
     assert main.check_full_word("coelho", "ventilador") == False
 
 
-test_check_letter()
+# test_check_letter()
 # test_add_letter()
-test_check_full_word()
+# test_check_full_word()
